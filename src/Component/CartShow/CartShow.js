ef982f0b1b0d0bfac6e0dc670f3cdf571fdb79f4
Abu Saeid <abusaeid95@gmail.com>
2021-09-27 15:25:58 +0600

    cart issue

--- src/Component/CartShow/CartShow.js
+++ src/Component/CartShow/CartShow.js
@@ -7,8 +7,6 @@ const CartShow = (props) => {
         <div className='show-person'>
            <img src={image} alt="" /> 
            <h4>{name}</h4>
-           <h5>{role}</h5>
-
         </div>
     );
 };
