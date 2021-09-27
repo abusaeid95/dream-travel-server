d430252dd07ad6000ae3d10096cbe14b9ee16178
Abu Saeid <abusaeid95@gmail.com>
2021-09-27 15:45:37 +0600

    hover

--- src/Component/CartShow/CartShow.js
+++ src/Component/CartShow/CartShow.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './CartShow.css'
 
 const CartShow = (props) => {
-    const {name, image, role} = props.personinfo
+    const {name, image} = props.personinfo
     return (
         <div className='show-person'>
            <img src={image} alt="" /> 
