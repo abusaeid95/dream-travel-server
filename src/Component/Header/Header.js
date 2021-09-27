df9f3d8d494df8a6a41eb83c4839402c13a9da4b
Abu Saeid <abusaeid95@gmail.com>
2021-09-27 15:35:36 +0600

    icon add

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,16 +3,14 @@ import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <div className='header'>
+         <div className='header'>
                 <h1 className='big-text'>SDC</h1>
                 <div className='header2p'>
                     <h3 className='header-h'>Make Your Home Awesome!</h3>
                     <p className='header-p'>We are always ready to make you luxurious</p>
                     <h3 className='header-p'>Budget $20k</h3>
                 </div>
-            </div>
-        </div>
+        </div>    
     );
 };
 
